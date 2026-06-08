8bba81338ed5a7526e497062ed9ce83053f73366
Mohamed Abdi <khadar.abdi165@gmail.com>
2026-06-08 18:09:56 +0200

    revert a fix error

--- sm-core/src/main/java/com/salesmanager/core/business/services/search/SearchServiceImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/services/search/SearchServiceImpl.java
@@ -346,7 +346,7 @@ public class SearchServiceImpl implements com.salesmanager.core.business.service
 
 	private Map<String, String> variants(ProductVariant variant) {
 		if (variant == null)
-			return Collections.emptyList();
+			return Collections.emptyMap();
 
 		Map<String, String> variantMap = new HashMap<String, String>();
 		if (variant.getVariation() != null) {
